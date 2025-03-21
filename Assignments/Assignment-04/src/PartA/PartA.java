ae9432caeafa32bd2e7741e575db8b3373c9019c
5321Asher <ashervirgona@gmail.com>
2025-03-21 08:41:17 -0400

    added comments to both part A and B

--- Assignments/Assignment-04/src/PartA/PartA.java
+++ Assignments/Assignment-04/src/PartA/PartA.java
@@ -1,126 +1,152 @@
-package PartA;
+package PartA; //defines package
 
-import Media.*;
+import Media.*; //imports turtle displayer and turtle
 
+/**
+ * this program recursively draws branches which form into a pattern. depending on certain values these patterns can
+ * change easily. these variables include the order, length, and rotation values.
+ * Asher Virgona
+ * 8032492
+ */
 public class PartA {
+    Turtle pen = new Turtle(0); //to draw
+    TurtleDisplayer display = new TurtleDisplayer(pen, 400, 400); //to draw on
     
-    Turtle pen = new Turtle(0);
-    TurtleDisplayer display = new TurtleDisplayer(pen, 400, 400);
-    
+    /**
+     * execeutes the for loop to indicate the amount of instances and rotation between each instance the patter will
+     * repeat
+     */
     public PartA() {
         pen.penDown();
         display.waitForUser();
-        for (int i = 0; i < 7; i++) {
+        for (int i = 0; i < 7; i++) { //7 instances
             branch(10, 75);
-            pen.left(2 * Math.PI / 7);
+            pen.left(2 * Math.PI / 7); //PI/7 rotation
         }
-        
         display.close();
     }
     
+    /**
+     * the branch method recursively calls itself to branch off until the order of the original call == 0. the order is
+     * decremented each recursive call, and the length of the branch is split anyway each recursive call.
+     * @param order the amount of branches left in the original method call
+     * @param len the length of the current branch/ length of next call branch
+     */
     private void branch(int order, int len) {
+        // base case, tells the program to stop if there are no more branches to be drawn
         if (order <= 0) {
             return;
         }
         
+        //if no branches have taken place yet (speacial case for inner pattern
         if (order == 10) {
-            // Special pattern for main branches
+            //regular outer patter first
             pen.forward(len);
             
-            // Create branches at the end
             pen.right(Math.PI / 6);
             branch(order - 1, len / 2);
             branch(order - 1, len / 4);
             
-            pen.left(Math.PI / 6);
+            pen.left(Math.PI / 6); //recenter
             
             pen.left(Math.PI / 6);
             branch(order - 1, len / 2);
             branch(order - 1, len / 4);
             
-            pen.right(Math.PI / 6);
+            pen.right(Math.PI / 6); //recenter
             
             
-            pen.backward(len / 3);  // Go back to middle
-            
-            pen.right(Math.PI / 6);
-            branch(order - 1, len / 2);
-            branch(order - 1, len / 4);
-            pen.left(Math.PI / 6);
-            
-            pen.left(Math.PI / 6);
-            branch(order - 1, len / 2);
-            
-            branch(order - 1, len / 4);
-            pen.right(Math.PI / 6);
-            
+            //special pattern for inner snowflake
             pen.backward(len / 3);
             
-            // Create additional inner branches
-              // Go back to middle
+            pen.right(Math.PI / 6);
+            branch(order - 1, len / 2);
+            branch(order - 1, len / 4);
             
+            pen.left(Math.PI / 6); //recenter
+            
+            pen.left(Math.PI / 6);
+            branch(order - 1, len / 2);
+            branch(order - 1, len / 4);
+            
+            pen.right(Math.PI / 6); //recenter
+            
+            //go back another third of len
+            pen.backward(len / 3);
             
             pen.right(Math.PI / 6);
             branch(order - 1, len / 2);
             branch(order - 1, len / 4);
-            pen.left(Math.PI / 6);
+            
+            pen.left(Math.PI / 6); //recenter
             
             pen.left(Math.PI / 6);
             branch(order - 1, len / 2);
-            
             branch(order - 1, len / 4);
-            pen.right(Math.PI / 6);
             
-            pen.backward(len / 3);  // Go all the way back
+            pen.right(Math.PI / 6); //recenter
             
+            //all the way back
+            pen.backward(len / 3);
+            
+            //special patter for outer snowflake
             pen.forward(len / 3 + len);
+            
             pen.right(Math.PI / 6);
             branch(order - 1, len / 2);
             branch(order - 1, len / 4);
-            pen.left(Math.PI / 6);
+            
+            pen.left(Math.PI / 6); //recenter
             
             pen.left(Math.PI / 6);
             branch(order - 1, len / 2);
-            
             branch(order - 1, len / 4);
-            pen.right(Math.PI / 6);
             
+            pen.right(Math.PI / 6); //recenter
+            
+            //all the way back
             pen.backward(len / 3 + len);
             
+            //even further out the snoflake, outskirts of the pattern
             pen.forward(2*(len / 3) + len);
+            
             pen.right(Math.PI / 6);
-            
             branch(order - 1, len / 5);
-            pen.left(Math.PI / 6);
+            
+            pen.left(Math.PI / 6); //recenter
             
             pen.left(Math.PI / 6);
-            
-            
             branch(order - 1, len / 5);
-            pen.right(Math.PI / 6);
             
+            pen.right(Math.PI / 6); //recenter
+            
+            //all the way back
             pen.backward(2*(len / 3) + len);
         } else {
-            // Regular pattern for smaller branches
+            //regular recursive call if it's not the stem/first branch
             pen.forward(len);
             
             pen.right(Math.PI / 6);
             branch(order - 1, len / 2);
             branch(order - 1, len / 4);
             
-            pen.left(Math.PI / 6);
+            pen.left(Math.PI / 6); //recenter
             
             pen.left(Math.PI / 6);
             branch(order - 1, len / 2);
             branch(order - 1, len / 4);
             
-            pen.right(Math.PI / 6);
+            pen.right(Math.PI / 6); //recenter
             
+            //all the way back
             pen.backward(len);
         }
     }
     
-    
+    /**
+     * serves as an entry point into the program, it initializes and runs the PartA constructor
+     * @param args not used
+     */
     public static void main(String[] args) {
         new PartA();
     }
--- Assignments/Assignment-04/src/PartB/PartB.java
+++ Assignments/Assignment-04/src/PartB/PartB.java
@@ -1,25 +1,41 @@
-package PartB;
+package PartB; //defines package
 
-import BasicIO.ASCIIDataFile;
-import BasicIO.ASCIIOutputFile;
+import BasicIO.ASCIIDataFile; //for reading maze in
+import BasicIO.ASCIIOutputFile; //for printing solved maze
 
+
+/**
+ * This program takes a data file (maze) solves the maze using case checking and recursive methods, and prints the
+ * solved maze at the end. It requires the brock.jar library and a txt file to run. It also creates a file called
+ * out.txt on every run, if already in folder, it overwrites.
+ * <p>
+ * Asher Virgona
+ * 8032492
+ */
 public class PartB {
-    private char[][] blocked;
+    private char[][] blocked; //to check chars for valid positions
     private int height;
     private int width;
-    private final ASCIIOutputFile display = new ASCIIOutputFile("out.txt");
+    private final ASCIIOutputFile display = new ASCIIOutputFile("out.txt"); //output solved maze
     private int startX;
     private int startY;
     
-    
+    /**
+     * loads the maze into an array, gets random start coordinates, and starts the solving process. it then prints the
+     * solved maze
+     */
     public PartB() {
         loadMaze();
         start();
         solveMaze(startX, startY);
-        blocked[startX][startY] = 'S';
+        blocked[startX][startY] = 'S'; //starting marker
         printMaze();
     }
     
+    /**
+     * gets random x and y coordinates for the start position. if the position == # or E, (!=' ') it generates new
+     * position
+     */
     private void start() {
         startX = (int) (Math.random() * height);
         startY = (int) (Math.random() * width);
@@ -27,63 +43,88 @@ public class PartB {
         if (blocked[startX][startY] == ' ') {
             System.out.println("starting position " + startX + " " + startY);
         } else {
-            start();
+            start(); //recursive calls the method until valid position is found
         }
         
     }
     
+    /**
+     * loads the maze text file into an X*Y array, X and Y must be found on the first line of they text file seperated
+     * by a tab and the maze directly underneath
+     */
     private void loadMaze() {
-        ASCIIDataFile maze = new ASCIIDataFile("mz2.txt");
-        height = maze.readInt();
-        width = maze.readInt();
-        System.out.println(height + " " + width);
+        ASCIIDataFile maze = new ASCIIDataFile("mz2.txt"); //load txt file
+        height = maze.readInt(); //first int on txt
+        width = maze.readInt(); //second in seperated by first int with a tab
+        System.out.println(height + " " + width); //make sure valid array size
         blocked = new char[height][width];
         for (int i = 0; i < height; i++) {
-            String line = maze.readLine();
+            String line = maze.readLine(); //read whole row of array
             for (int j = 0; j < width && j < line.length(); j++) {
-                blocked[i][j] = line.charAt(j);
+                blocked[i][j] = line.charAt(j); //convert row into chars
             }
         }
     }
     
+    /**
+     * This method solves the maze using recursive logic. it tests each direction and returns true if direction is
+     * valid, the directions are represented using ASCII < (left), > (right), ^ (up), V (down).if any of the cases such
+     * as # or . are seen it returns false. if no directions are valid, the method prints a '.' and returns false. if
+     * current coordinate == E the program ends as you have found the exit.
+     *
+     * @param x the X coordinate to check for a valid direction/position
+     * @param y the Y coordinate to check for a valid direction/position
+     * @return returns true if the position is valid, false if the position is invalid.
+     */
     private boolean solveMaze(int x, int y) {
+        //win case
         if (blocked[x][y] == 'E') {
             System.out.println("you win");
             return true;
         }
         
-        if  (x >= height || y >= width) {
+        //edge case
+        if (x >= height || y >= width) {
             return false;
         }
         
+        //valid position
         if (blocked[x][y] == '#' || blocked[x][y] == '.' || blocked[x][y] == '^' || blocked[x][y] == 'v' || blocked[x][y] == '<' || blocked[x][y] == '>' || blocked[x][y] == 'S') {
             return false;
         }
         
+        //left
         blocked[x][y] = '<';
         if (solveMaze(x, y - 1)) {
             return true;
         }
         
+        //down
         blocked[x][y] = 'V';
         if (solveMaze(x + 1, y)) {
             return true;
         }
         
+        //right
         blocked[x][y] = '>';
         if (solveMaze(x, y + 1)) {
             return true;
         }
         
+        //up
         blocked[x][y] = '^';
         if (solveMaze(x - 1, y)) {
             return true;
         }
         
+        //no valid positions
         blocked[x][y] = '.';
         return false;
     }
     
+    /**
+     * Prints array to output file, used to print the final solved maze to output file.
+     */
     private void printMaze() {
         for (int i = 0; i < height; i++) {
             for (int j = 0; j < width; j++) {
@@ -93,6 +134,11 @@ public class PartB {
         }
     }
     
+    /**
+     * serves as an entry point for the program, it initializes and runs the PartB constructor
+     *
+     * @param args not used
+     */
     public static void main(String[] args) {
         new PartB();
     }
